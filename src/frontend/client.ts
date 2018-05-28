0ad526806b92d3c1eff177b58185b5bd06c780e7
felipeMello <felipe.silva.mello@gmail.com>
2018-05-28 13:42:48 +0100

    front end

--- src/frontend/client.ts
+++ src/frontend/client.ts
@@ -1,43 +1,44 @@
 //Global scope
-let TOKEN ="";
+let TOKEN = "";
 
-let createUser = (user) =>{
+//createUser
+let createUser = (user) => {
     const response = fetch("api/v1/users", {
         method: "POST",
-        headers : {
+        headers: {
             "Accpet": "application/json",
-            "Content-Type":"application/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(user)
-    }).then(response =>{
+    }).then(response => {
         response.json().then(json => {
             console.log(json);
         });
     })
 };
 
-//createUser
 
-let getAuthToken = async(user) =>{
-    const response = fetch("api/v1/auth/login",{
-        method : "POST",
-        headers : {
+// get AuthToken
+let getAuthToken = async (user) => {
+    const response = fetch("api/v1/auth/login", {
+        method: "POST",
+        headers: {
             "Accpet": "application/json",
-            "Content-Type":"application/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(user)
-    }).then(response =>{
-        response.json().then(json =>{
+    }).then(response => {
+        response.json().then(json => {
             //The first request
-        TOKEN = json.token;
-        console.log(json);
+            TOKEN = json.token;
+            console.log(json);
         });
-        
+
     })
 };
 
-// get AuthToken
 
+//create a Link
 let createLink = async (link) => {
     const response = fetch(
         "/api/v1/links",
@@ -47,7 +48,7 @@ let createLink = async (link) => {
                 "Authorization": TOKEN, // We reed the token from the global variable (we must call getAuthToken first)
                 "Accept": "application/json",
                 "Content-Type": "application/json"
-            },  
+            },
             body: JSON.stringify(link)
         }
     ).then(response => {
@@ -56,3 +57,69 @@ let createLink = async (link) => {
         });
     })
 };
+
+//getAll links
+let getLinks = async () => {
+    const response = fetch("api/v1/links/", {
+        method: "GET"
+    }).then(response => {
+        response.json().then(json => {
+            console.log(json);
+        });
+    })
+};
+
+//deleteLink
+let deleteLink = async (linkId) => {
+    const response = fetch(`api/v1/links/${linkId}`, {
+        method: "DELETE",
+        headers: {
+            //We read the token from the global variable (we must call getAuthToken first)
+            "Authorization": TOKEN
+        }
+    }).then(response => {
+        response.json().then(json => {
+            console.log(json);
+        });
+    })
+}
+
+//upvote
+let upvoteLink = (linkId) => {
+    const response = fetch(`/api/v1/links/${linkId}/upvote`, {
+        method: "POST",
+        headers: {
+            //We read the token from the global variable (we must call getAuthToken first)
+            "Authorization ": TOKEN
+        }
+    }).then(response => {
+        response.json().then(json => {
+            console.log(json);
+        });
+    })
+};
+
+let downVoteLink = (linkId) => {
+    const response = fetch(`/api/v1/links/${linkId}/downvote`, {
+        method: "POST",
+        headers: {
+            "Authorization": TOKEN
+
+        }
+    }).then(response => {
+        response.json().then(json => {
+            console.log(json);
+        })
+
+    });
+};
+
+let getVotesForLink = (linkId) =>{
+    const response = fetch(`/api/v1/links/${linkId}/votes`,{
+        method: "GET"
+    }).then(response =>{
+        response.json().then(json =>{
+            console.log(json);
+        })
+    })
+}
